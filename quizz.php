2571f7b0eafd5a545f423d1d7987dd2a6b3ba341
Drushti <ramtekedrushti28@gmail.com>
2024-10-06 20:17:34 +0700

    Add files via upload

---
+++ quizz.php
@@ -0,0 +1,39 @@
+
+
+<html>
+<head>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="quizz.css">
+</head>
+<body>
+	<center>
+	<div>
+		<form action="quizz1.php" method="post">
+		
+		<h1>Welcome To Quizzes World
+			</h1>
+	
+		<h2>Instructions:</h2>
+		<ol>
+
+			<li>Read each question carefully before answering.</li>
+			<li>Select the best answer from the given options.</li>
+			<li>Each question is mandatory.Make sure to answer all questions.</li>
+			<li>Once submitted, your answer cannot be changed.</li>
+			
+			</ol>
+	
+		
+
+		<a href="quizz1.php">
+		<input type="submit" value="next ->" name="btn_next"></a>
+
+
+
+
+	</form>
+</div>
+</center>
+
+</body>
+</html>
